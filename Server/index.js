2c9d43d7566f0b654f43554fa0a2332a26dc5de0
RefaelBZ <refael747@gmail.com>
2024-08-31 23:42:40 +0300

    index

--- Server/index.js
+++ Server/index.js
@@ -2,20 +2,19 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-
 const db = require("./db");
 db.connect();
 
 const cors = require('cors');
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: process.env.CLIENT_ORIGIN || '*',
   credentials: true
 }));
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send('Hello world!');
 });
 
 const workoutRouter = require('./workout/workoutRouter');
@@ -23,7 +22,6 @@ app.use('/workout', workoutRouter);
 
 const userRouter = require('./User/user.router')
 app.use("/user", userRouter)
-
 
 const PORT = process.env.PORT || 2500;
 app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}!`));
