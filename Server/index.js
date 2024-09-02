2db932d17c94eb30147a1c87d7d32cfaf4b4fb31
RefaelBZ <refael747@gmail.com>
2024-09-02 23:44:12 +0300

    a

--- Server/index.js
+++ Server/index.js
@@ -7,18 +7,10 @@ db.connect();
 
 const cors = require('cors');
 
-// app.use(cors({
-//   origin: ['https://logym.vercel.app', 'http://localhost:5173'],
-//   credentials: true
-// }));
-
-
-app.use(cors());
-
-
-
-
-
+app.use(cors({
+  origin: ['https://logym.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}));
 
 app.use(express.json());
 
