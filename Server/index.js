59dc0153556f3a530b565a2c5060ab66930e95e1
RefaelBZ <refael747@gmail.com>
2024-09-12 22:01:13 +0300

    fix user log in

--- Server/index.js
+++ Server/index.js
@@ -7,20 +7,32 @@ db.connect();
 
 const cors = require('cors');
 
-
 app.use(cors({
-  origin: ['https://logym.vercel.app', 'http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://flourishing-dusk-595691.netlify.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
 app.use(express.json());
 
+app.options('*', cors());
+
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send('Hello, world!');
 });
 
-const workoutRouter = require('./workout/workoutRouter');
-app.use('/workout', workoutRouter);
+const likedRouter = require('./Liked/liked.route');
+app.use('/liked', likedRouter);
+
+const topRouter = require('./Top/top.router');
+app.use('/top', topRouter);
+
+const craftedRouter = require('./Crafted/Crafted.router');
+app.use('/crafted', craftedRouter);
+
+const trendingRouter = require('./Trending/Trending.router');
+app.use('/trending', trendingRouter);
 
 const userRouter = require('./User/user.router')
 app.use("/user", userRouter)
