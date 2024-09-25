83c5e6119e46bbee7a7c258158f2bf19e6589f42
RefaelBZ <refael747@gmail.com>
2024-09-25 21:13:43 +0300

    Fix cancelEdit

--- Client/src/pages/EditWorkout/index.jsx
+++ Client/src/pages/EditWorkout/index.jsx
@@ -151,7 +151,6 @@ export default function EditWorkout({ setWorkouts }) {
           <EditExerciseForm
             exercise={editingExercise}
             onFormDataSubmit={handleSaveExercise}
-            onCancel={handleCancelEdit}
           />
         )}
       </div>
